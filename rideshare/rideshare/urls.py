9f88c79a2bc2f33bce6888923db9392292092269
vedangj044 <vedangj044@gmail.com>
2019-02-15 09:57:59 +0530

    Edited Admin Bar with custom name "ride-share", Made migrations

--- rideshare/rideshare/urls.py
+++ rideshare/rideshare/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+admin.site.site_header = "Ride Share | Login"
+admin.site.site_title = "Ride Share Admin Portal"
+admin.site.index_title = "Welcome to RideShare"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
